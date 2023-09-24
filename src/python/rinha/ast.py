0964e7b4847203cb83a5bf9dd21827c6f850bcfb
Diego Ramos Ruggeri <diego@ruggeri.net.br>
2023-09-24 13:23:03 -0300

    Add types to AST model

--- src/python/rinha/ast.py
+++ src/python/rinha/ast.py
@@ -1,3 +1,4 @@
+from math import fmod
 from rply.token import BaseBox
 from rply.token import Token
 
@@ -7,7 +8,17 @@ class Term(BaseBox):
     def eval(self):
         raise NotImplementedError
 
-## Primitive values
+## Literal values
+
+class Int(Term):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        str(self.value)
+
+    def eval(self):
+        return self.value
 
 class Str(Term):
     def __init__(self, value):
@@ -19,14 +30,41 @@ class Str(Term):
     def eval(self):
         return self.value
 
-class Int(Term):
+class Bool(Term):
     def __init__(self, value):
         self.value = value
 
     def __str__(self):
-        "%d" % self.value
+        return str(self.value)
+
+    def eval(self):
+        return self.value
+
+class Tuple(Term):
+    def __init__(self, first, second):
+        self.value = (first, second)
+
+    def __str__(self):
+        return str(self.value)
+
+    def eval(self):
+        return self.value
 
 # User defined functions
+
+class Function(Term):
+    def __init__(self, params, expr):
+        self.params = params
+        self.expr = expr
+
+    def __str__(self):
+        return "fn ({}) => {}".format(_join(self.params), self.expr)
+
+    def eval(self):
+        return self.value
+
+    def invoke(self, args):
+        pass
 
 class Call(Term):
     def __init__(self, callee, args):
@@ -34,10 +72,10 @@ class Call(Term):
         self.args = args
 
     def __str__(self):
-        pass
-    
-    def eval():
-        pass
+        return "Call {} with args: {}".format(self.callee, _join(self.args))
+
+    def eval(self):
+        return self.callee.invoke(self.args)
 
 ## Intrinsic functions
 
@@ -49,8 +87,22 @@ class Print(Term):
         value = self.expr.eval()
         print(value)
         return value
-    
-### Binary operations
+
+class First(Term):
+    def __init__(self, tuple):
+        self.tuple = tuple
+
+    def eval(self):
+        return tuple[0]
+
+class Second(Term):
+    def __init__(self, tuple):
+        self.tuple = tuple
+
+    def eval(self):
+        return tuple[1]
+
+### Binary operators
 
 class Binary(Token):
     def __init__(self, left, right):
@@ -72,4 +124,69 @@ class Mul(Binary):
 class Div(Binary):
     def eval(self):
         return self.left.eval() / self.right.eval()
+
+class Rem(Binary):
+    def eval(self):
+        return fmod(self.left.eval(), self.right.eval())
+
+class Eq(Binary):
+    def eval(self):
+        return self.left.eval() == self.right.eval()
     
+class Neq(Binary):
+    def eval(self):
+        return self.left.eval() != self.right.eval()
+
+class Lt(Binary):
+    def eval(self):
+        return self.left.eval() < self.right.eval()
+
+class Gt(Binary):
+    def eval(self):
+        return self.left.eval() > self.right.eval()
+
+class Lte(Binary):
+    def eval(self):
+        return self.left.eval() <= self.right.eval()
+
+class Gte(Binary):
+    def eval(self):
+        return self.left.eval() >= self.right.eval()
+
+class And(Binary):
+    def eval(self):
+        return self.left.eval() and self.right.eval()
+
+class Or(Binary):
+    def eval(self):
+        return self.left.eval() or self.right.eval()
+
+### Flow control
+
+class Let(Term):
+    def __init__(self, expr, args):
+        self.callee = callee
+        self.args = args
+
+    def __str__(self):
+        pass
+
+    def eval():
+        pass
+
+class If(Term):
+    def __init__(self, condition, then, otherwise):
+        self.condition = condition
+        self.then = then
+        self.otherwise = otherwise
+
+    def __str__(self):
+        pass
+
+    def eval():
+        pass
+
+### Helper functions
+
+def _join(it):
+    return ','.join(it)
--- src/python/rinha/parsing.py
+++ src/python/rinha/parsing.py
@@ -12,6 +12,11 @@ def term_number(tokens):
     string, = tokens
     return ast.Str(string.getstr())
 
+@pg.production('term : DIGITS')
+def term_number(tokens):
+    string, = tokens
+    return ast.Int(int(string.getstr()))
+
 @pg.production('term : PRINT OPEN_PARENS term CLOSE_PARENS')
 def print_term(tokens):
     print_call, lparen, term, rparen = tokens
